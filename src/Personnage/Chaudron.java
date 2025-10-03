dc2c3053de04b3b8a7a139a566ceae4da8e3dcf4
Brawnyh <amadou@LORDI-EI2BGPO41.localdomain>
2025-10-03 21:06:06 +0200

    TP1 corriger

--- src/Personnage/Chaudron.java
+++ src/Personnage/Chaudron.java
@@ -3,26 +3,32 @@ package Personnage;
 public class Chaudron {
 	private int quantitePotion;
 	private int forcePotion;
+	
 	public Chaudron(int quantitePotion,int forcePotion) {
 		this.quantitePotion = quantitePotion;
 		this.forcePotion = forcePotion;
 	}
 	
 	public void remplirChaudron(int quantite , int forceP) {
-		quantitePotion=quantitePotion + quantite;
-		forcePotion = forcePotion + forceP ;
+		this.quantitePotion= this.quantitePotion+quantite;
+		this.forcePotion = this.forcePotion +forceP ;
 	}
 	
 	public boolean resterPotion() {
-		if (quantitePotion<0) {
-			return false;
-		}else {
-			return true;
-		}
-	
+		return quantitePotion>0;
 	}
 	
 	public int prendreLouche() {
+		if ( quantitePotion>0) {
+			quantitePotion -=1;
+		}
+		
+		return quantitePotion;
+		
+	}
+	
+	public void parler(String texte) {
+		System.out.println("J'ai concocté" + quantitePotion + "doses de potion magique. Elle a une force de " + forcePotion + ".");
 		
 	}
 }
--- src/Personnage/Druide.java
+++ src/Personnage/Druide.java
@@ -3,18 +3,35 @@ package Personnage;
 public class Druide {
 	private String nom;
 	private int force;
-	
+	private Chaudron chaudron = new Chaudron(0,0);
 	public Druide(String nom,int force) {
 		this.nom=nom;
 		this.force=force;
-		
+		this.chaudron = chaudron;
 	}
 	
 	public String getNom() {
 		return nom;
 	}
 	
-	public void fabriquerPotion(int quantite,int forcePotion) {
+	public void fabriquerPotion(int quantite,int forceP) {
+		chaudron.remplirChaudron(quantite,forceP);
+		parler("J'ai concocté" + " " + quantite + " " + "doses de potion magique. Elle a une force de " + forceP + ".");
+	}
+	
+	public void booster(Gaulois gaulois) {
+		if (chaudron.resterPotion()) {
+			
+			if (gaulois.getNom() == "Obélix") {
+				parler("Non, " + gaulois.getNom() + " ! Et tu le sais très bien !");
+			}else {
+				gaulois.boirePotion(chaudron.prendreLouche());
+				parler("Tiens " + gaulois.getNom() + ", voilà une louche de potion !");
+			}
+			
+	    } else {
+	    	parler("Désolé " + gaulois.getNom() + ", il n'y a plus une seule goutte de potion.");
+		}
 		
 	}
 
@@ -25,6 +42,8 @@ public class Druide {
 	private String prendreParole() {
 		return "Le Druide " + nom + " : ";
 	}
+	
+	
 	
 	
 
